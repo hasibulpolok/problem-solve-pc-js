8d438a5567fcb6fee8b9fd7cb44f813d5517a10c
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-02-28 10:18:28 +0600

    js object function

---
+++ js_object.js
@@ -0,0 +1,19 @@
+// const user = {
+//   name: "Polok",
+//   greet: function () {
+//     // return(`My name is ${this.name}`);
+//     console.log(`My name is ${this.name}`)
+//   }
+// };
+
+// user.greet();
+
+const user2 = {
+    name:"Ridu",
+    age:25,
+    ridu: function(){
+        console.log(`My name is ${this.name}`)
+    }
+}
+
+user2.ridu();
